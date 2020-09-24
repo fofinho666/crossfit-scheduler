d262dedb924cff4727e0e91dc9a88fcd413253b1
fofinho666 <a_tralha_do_hugo@hotmail.com>
2020-09-24 20:47:53 +0100

    🗄 Add mongodb driver to the project

--- index.js
+++ index.js
@@ -4,28 +4,36 @@ const express = require('express');
 const basicAuth = require('express-basic-auth');
 const Agenda = require('agenda');
 const Agendash = require('agendash');
+const { MongoClient } = require("mongodb");
 
-const app = express();
-const agenda = new Agenda({ db: { address: process.env.MONGODB_URI, options: { useUnifiedTopology: true } } });
+async function main() {
+  const appTitle = "CrossFit Scheduler";
 
-const appTitle = "CrossFit Scheduler";
-const port = parseInt(process.env.PORT)
+  const port = parseInt(process.env.PORT)
 
-agenda.define('CrossFit Class', async job => {
-  const { local, hour, daysInAdvance } = job.attrs.data;
-  await booking_class.automation(local, hour, daysInAdvance);
-});
-agenda.define('Weightlifting Class', async job => {
-  const { local, hour, daysInAdvance } = job.attrs.data;
-  await booking_class.automation(local, hour, daysInAdvance);
-});
-agenda.define('Gymnastics Class', async job => {
-  const { local, hour, daysInAdvance } = job.attrs.data;
-  await booking_class.automation(local, hour, daysInAdvance);
-});
+  const app = express();
+  const client = await MongoClient.connect(process.env.MONGODB_URI, { useUnifiedTopology: true });
+  const agenda = new Agenda({mongo: client.db('agenda')});
 
-(async function () {
-  await agenda.start();
+  agenda.define('Get Cookie', async job => {
+    await booking_class.automation(local, hour, daysInAdvance);
+  });
+  agenda.define('CrossFit Class', async job => {
+    const { local, hour, daysInAdvance } = job.attrs.data;
+    await booking_class.automation(local, hour, daysInAdvance);
+  });
+  agenda.define('Weightlifting Class', async job => {
+    const { local, hour, daysInAdvance } = job.attrs.data;
+    await booking_class.automation(local, hour, daysInAdvance);
+  });
+  agenda.define('Gymnastics Class', async job => {
+    const { local, hour, daysInAdvance } = job.attrs.data;
+    await booking_class.automation(local, hour, daysInAdvance);
+  });
+
+  // Wait for agenda to connect. Should never fail since connection failures
+  // should happen in the `await MongoClient.connect()` call.
+  await new Promise(resolve => agenda.once('ready', resolve));
 
   const crossfitClassReservation = {
     local: "Rato",
@@ -62,13 +70,18 @@ agenda.define('Gymnastics Class', async job => {
     gymnasticsClassReservation,
     { timezone: 'Europe/Lisbon' }
   );
+  agenda.start();
 
-})();
+  let basicAuthUsers = {};
+  basicAuthUsers[process.env.AUTH_USERNAME] = process.env.AUTH_PASSWORD
+  app.use(basicAuth({ users: basicAuthUsers, challenge: true }));
 
-let basicAuthUsers = {};
-basicAuthUsers[process.env.AUTH_USERNAME] = process.env.AUTH_PASSWORD
-app.use(basicAuth({ users: basicAuthUsers, challenge: true }));
+  app.use('/', Agendash(agenda, { title: appTitle }));
 
-app.use('/', Agendash(agenda, { title: appTitle }));
+  app.listen(port, () => console.log(`${appTitle} listening at http://localhost:${port}`))
+}
 
-app.listen(port, () => console.log(`${appTitle} listening at http://localhost:${port}`))
+main().catch(error => {
+  console.error(error);
+  process.exit(-1);
+});
