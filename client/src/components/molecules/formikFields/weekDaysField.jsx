56b5917b7d72ab848a7071944c114cb8db0503dc
fofinho666 <a_tralha_do_hugo@hotmail.com>
2021-07-28 14:30:52 +0100

    Style checkboxes

--- client/src/components/molecules/formikFields/weekDaysField.jsx
+++ client/src/components/molecules/formikFields/weekDaysField.jsx
@@ -14,8 +14,8 @@ const WeekDaysField = () => {
   }
 
   const labelClass = (weekDaysMeta.touched && weekDaysMeta.error)
-    ? "checkbox has-text-danger"
-    : "checkbox"
+    ? "b-checkbox checkbox has-text-danger"
+    : "b-checkbox checkbox"
 
   const WeekDaysError = () => {
     if (weekDaysMeta.touched && weekDaysMeta.error) {
@@ -27,13 +27,13 @@ const WeekDaysField = () => {
   return <>
     <div className="label" label="checkbox-group">Job Days</div>
     <div className="columns is-mobile is-multiline is-centered" role="group" aria-labelledby="checkbox-group">
-      <div className="column is-narrow"><label className={labelClass}> <Field type="checkbox" name="weekDays" value="1" validate={validateWeekDays} /> Monday </label></div>
-      <div className="column is-narrow"><label className={labelClass}> <Field type="checkbox" name="weekDays" value="2" validate={validateWeekDays} /> Tuesday </label></div>
-      <div className="column is-narrow"><label className={labelClass}> <Field type="checkbox" name="weekDays" value="3" validate={validateWeekDays} /> Wednesday </label></div>
-      <div className="column is-narrow"><label className={labelClass}> <Field type="checkbox" name="weekDays" value="4" validate={validateWeekDays} /> Thursday </label></div>
-      <div className="column is-narrow"><label className={labelClass}> <Field type="checkbox" name="weekDays" value="5" validate={validateWeekDays} /> Friday </label></div>
-      <div className="column is-narrow"><label className={labelClass}> <Field type="checkbox" name="weekDays" value="6" validate={validateWeekDays} /> Saturday </label></div>
-      <div className="column is-narrow"><label className={labelClass}> <Field type="checkbox" name="weekDays" value="0" validate={validateWeekDays} /> Sunday </label></div>
+      <div className="column is-narrow pb-0"><label className={labelClass}> <Field type="checkbox" name="weekDays" value="1" validate={validateWeekDays} /> <span className="check"></span> <span className="control-label">Monday</span></label></div>
+      <div className="column is-narrow pb-0"><label className={labelClass}> <Field type="checkbox" name="weekDays" value="2" validate={validateWeekDays} /> <span className="check"></span> <span className="control-label">Tuesday </span></label></div>
+      <div className="column is-narrow pb-0"><label className={labelClass}> <Field type="checkbox" name="weekDays" value="3" validate={validateWeekDays} /> <span className="check"></span> <span className="control-label">Wednesday </span></label></div>
+      <div className="column is-narrow pb-0"><label className={labelClass}> <Field type="checkbox" name="weekDays" value="4" validate={validateWeekDays} /> <span className="check"></span> <span className="control-label">Thursday </span></label></div>
+      <div className="column is-narrow pb-0"><label className={labelClass}> <Field type="checkbox" name="weekDays" value="5" validate={validateWeekDays} /> <span className="check"></span> <span className="control-label">Friday </span></label></div>
+      <div className="column is-narrow pb-0"><label className={labelClass}> <Field type="checkbox" name="weekDays" value="6" validate={validateWeekDays} /> <span className="check"></span> <span className="control-label">Saturday </span></label></div>
+      <div className="column is-narrow pb-0"><label className={labelClass}> <Field type="checkbox" name="weekDays" value="0" validate={validateWeekDays} /> <span className="check"></span> <span className="control-label">Sunday </span></label></div>
     </div>
     <WeekDaysError />
   </>
